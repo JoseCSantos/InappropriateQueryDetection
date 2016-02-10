7d7c87a4500588188a0a40b28dc5c5121e033c19
parthg <parthg.88@gmail.com>
2016-02-10 17:32:02 +0100

    adding dssm

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+lib="lib";
+jcuda_home="/home/parth/workspace/JCuda-All-0.5.5-bin-linux-x86_64";
 if [ $# -gt 0 ]; then
-  java -Xmx4G -ea -classpath "bin/" $*
+  java -Xmx28G -Duse_cuda=true -ea -classpath bin/:$lib/trove-3.0.1.jar:$lib/jblas-1.2.3-SNAPSHOT.jar:$lib/terrier-3.5-core.jar:$lib/antlr.jar:$lib/mallet.jar:$lib/opennlp-tools-1.5.0.jar:$lib/maxent-3.0.0.jar:$lib/Nemo-20150912.jar:$lib/hadoop-0.20.2+228-core.jar:$lib/log4j-1.2.15.jar:$lib/snowball-20071024.jar:$lib/trove-2.0.2.jar:$lib/commons-logging-1.1.1.jar:$lib/cl-deep-20150204.jar:$jcuda_home/jcuda-0.5.5.jar:$jcuda_home/jcublas-0.5.5.jar:$lib/jDNN-20160210.jar $*
 else
   echo "Your command line contains no arguments"
 fi
